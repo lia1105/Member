1f9248c95bb427693a7b3b89bae3377ad4688c94
miia <miia@com.tw>
2020-01-07 15:04:21 +0800

    1st

--- app/src/main/java/com/fju/member/MainActivity.java
+++ app/src/main/java/com/fju/member/MainActivity.java
@@ -2,13 +2,41 @@ package com.fju.member;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView showname;
+    private TextView showage;
+    private TextView showgender;
+    public Button button;
+    private Intent intentuser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);//
+        setContentView(R.layout.activity_main);
+        showname = findViewById(R.id.nickname);
+        showgender = findViewById(R.id.gender);
+        showage = findViewById(R.id.age);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intentuser = new Intent(MainActivity.this, NicknameActivity.class);
+                startActivity(intentuser);
+            }
+        });
+
+
+
+
     }
+
+
 }
--- app/src/main/java/com/fju/member/NicknameActivity.java
+++ app/src/main/java/com/fju/member/NicknameActivity.java
@@ -3,12 +3,19 @@ package com.fju.member;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NicknameActivity extends AppCompatActivity {
+
+    private EditText name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nickname);
+
+        name = findViewById(R.id.nickname);
+
     }
 }
