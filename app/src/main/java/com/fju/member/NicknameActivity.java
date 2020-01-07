7ba958897db72684d8fc012cd7a5c3eb6ffaac3a
miia <miia@com.tw>
2020-01-07 15:10:09 +0800

    2nd

--- app/src/main/java/com/fju/member/NicknameActivity.java
+++ app/src/main/java/com/fju/member/NicknameActivity.java
@@ -2,13 +2,16 @@ package com.fju.member;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class NicknameActivity extends AppCompatActivity {
 
     private EditText name;
+    Button nextage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,14 @@ public class NicknameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_nickname);
 
         name = findViewById(R.id.nickname);
+        nextage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentage = new Intent(NicknameActivity.this, AgeActivity.class);
+                intentage.putExtra("user name", name.getText().toString());
+                startActivity(intentage);
+            }
+        });
 
     }
 }
